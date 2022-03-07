6e2270c296b93a8a493aff313a84f92b8a9a481f
Ana Echeverri <97066004+anaecheverri@users.noreply.github.com>
2022-03-07 11:36:21 -0500

    Update Header.js

--- src/template/Header.js
+++ src/template/Header.js
@@ -2,7 +2,7 @@ const header = () => {
   const view = `
     <div class="Header-main">
       <div class="Header-logo">
-          <a href="/"> <img src="./src/images/logo.png" alt="logo"></a>
+          <a href="/"> <img src="https://occ-0-1723-1722.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABddiw4GEUq76B3fmiI7r6NF-GrWeEf99MjwKrfixFKM4B4o1uuitcgbuBNa3n04L5GSamUi2vex4adduBV-S2XGERxn29-ffvoRv.png?r=a6e" alt="logo"></a>
       </div>
       <div class="Header-nav">
         <h1>
